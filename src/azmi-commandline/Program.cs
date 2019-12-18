ffee5b385958468b5281521fcee0b8b3771457ef
Igor Iric <iiric@microsfot.com>
2019-12-18 17:59:25 +0100

    args count check

--- src/azmi-commandline/Program.cs
+++ src/azmi-commandline/Program.cs
@@ -7,7 +7,7 @@ namespace azmi_commandline
     {
         static void Main(string[] args)
         {
-            if (String.IsNullOrEmpty(args[0]))
+            if (args.Length == 0)
             {
                 // display usage and error
                 HelpMessage.application();
@@ -19,16 +19,19 @@ namespace azmi_commandline
                 // display usage
                 HelpMessage.application();
             }
-            else if (String.IsNullOrEmpty(args[1]) && args[1] == "help") {
+            else if (args.Length == 2 && args[1] == "help") {
+                // Command specific help. like "azmi setblob help"
                 HelpMessage.command(args[0]);
+                // TODO: Verify if args[0] is defined function
             }
             else if (args[0] == "setblob") {
                 
                 //
                 // set blob command
+                // azmi setblob $BLOB $FILE
                 //
                 
-                if (String.IsNullOrEmpty(args[1]) || String.IsNullOrEmpty(args[2]))
+                if (args.Length != 3)
                 {
                     // requires parameters error, display setblob usage                    
                     HelpMessage.command("setblob");
