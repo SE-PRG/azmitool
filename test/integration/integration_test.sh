3b7a825a1a8a5106dc538a2cc9590daf3d278099
Igor Iric <iiric@microsoft.com>
2020-03-19 10:25:14 +0100

    adding script arguments

--- test/integration/integration_test.sh
+++ test/integration/integration_test.sh
@@ -9,11 +9,8 @@
 #
 
 # TODO: remove these two from script, or change it to $1, $2
-STORAGEACCOUNTNAME=azmitest
-identity=354800af-354e-42e0-906b-5b96e02c4e1c
-echo "First argument: '$1'"
-echo "Second argument: '$2'"
-echo "Third argument: '$3'"
+STORAGEACCOUNTNAME=$2
+identity=$3
 
 
 export DEBIAN_FRONTEND=noninteractive
