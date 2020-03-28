262ea258cbb1f34dffae6d69297795a726f88a9f
Igor Iric <iiric@microsoft.com>
2020-03-28 22:34:46 +0100

    minor commit cleanup, re-add launchSettings

--- src/azmi-main/AzmiOption.cs
+++ src/azmi-main/AzmiOption.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace azmi_main
 {
