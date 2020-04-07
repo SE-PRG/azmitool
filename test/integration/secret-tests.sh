6273eea13ec01227a3862a2cfe44dd648f9fd03a
Igor Iric <iiric@microsoft.com>
2020-04-07 12:40:29 +0200

    second split

---
+++ test/integration/secret-tests.sh
@@ -0,0 +1,17 @@
+#
+# secret subcommands testing
+#
+
+KV_BASENAME="azmi-itest"
+KV_NA="https://${KV_BASENAME}-no-access.vault.azure.net"
+KV_RO="https://${KV_BASENAME}-r.vault.azure.net"
+
+testing class "getsecret"
+test "getsecret fails on existing but foreign secret" assert.Fail "azmi getsecret --secret ${KV_NA}/secrets/buriedSecret"
+test "getsecret OK on RO secret" assert.Equals "azmi getsecret --secret ${KV_RO}/secrets/readMyPassword --identity $identity" "LikeThat"
+
+test "getsecret fails on missing secret" assert.Fail "azmi getsecret --secret ${KV_RO}/secrets/iDoNotExist --identity $identity"
+test "getsecret fails on invalid URL #1" assert.Fail "azmi getsecret --secret ${KV_RO}"
+test "getsecret fails on invalid URL #2" assert.Fail "azmi getsecret --secret ${KV_RO}/"
+test "getsecret fails on invalid URL #3" assert.Fail "azmi getsecret --secret http://azmi-itest-r.vault.azure.net/secrets/readMyPassword"   # http protocol
+test "getsecret fails on invalid URL #4" assert.Fail "azmi getsecret --secret https:\\\azmi-itest-r.vault.azure.net/secrets/readMyPassword" # backslashes
