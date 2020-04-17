d5669fa94fb30dc4f05b466219eda366bf32fe72
MangyCoyote <43244941+MangyCoyote@users.noreply.github.com>
2020-04-17 12:33:36 +0200

    win vs *nix sha256sums

--- test/integration/certificate-tests.sh
+++ test/integration/certificate-tests.sh
@@ -22,10 +22,10 @@ KV_RO="https://${KV_BASENAME}-r.vault.azure.net"
 test "getcertificate fails on existing but foreign certificate" assert.Fail "azmi getcertificate --certificate ${KV_NA}/certificates/buriedCertificate"
 
 test "getcertificate OK on RO latest PEM certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificate --identity $identity | grep MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCdJzK88AQzWXVO"
-test "getcertificate OK on RO latest PFX certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat --identity $identity | sha256sum  938ed434f69fa10e4e3af656ef089e684a92772a07b79b6af8f3134a025fd65b"
+test "getcertificate OK on RO latest PFX certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat --identity $identity | sha256sum  f3f7af1b9fbaa5bfbd54ac21e06af2fb0be37d1766ed11f305f83a55b2623842"
 
 test "getcertificate OK on RO specific version of PEM certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificate/103a7355c6094bc78307b2db7b85b3c2 --identity $identity | grep MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCvga/z9gy4RG0S"
-test "getcertificate OK on RO specific version of PFX certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat/035f5dc633a445bb801f760f46286785 --identity $identity | sha256sum ed197593c7d293427ed2c9138fd6b52e5ffcad1e1b824195762e1c7ffc00a906"
+test "getcertificate OK on RO specific version of PFX certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat/035f5dc633a445bb801f760f46286785 --identity $identity | sha256sum 1ffd2ed6d8e87997adb4a172b3ff161dd69bba7113a3c16ecc6237cbd749bd0c"
 
 test "getcertificate fails on non-existing specific version of certificate" assert.Fail "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificate/xxxxxxxVersionDoesNotExistxxxxxx --identity $identity"
 test "getcertificate fails on missing certificate" assert.Fail "azmi getcertificate --certificate ${KV_RO}/certificates/iDoNotExist --identity $identity"
