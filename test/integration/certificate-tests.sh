59a3d22e71dd651b4267229c62f23fb55a098938
MangyCoyote <43244941+MangyCoyote@users.noreply.github.com>
2020-04-30 14:48:29 +0200

    iTest abs, rel pfx -> pem

--- test/integration/certificate-tests.sh
+++ test/integration/certificate-tests.sh
@@ -23,10 +23,10 @@ testing class "getcertificate"
 test "getcertificate fails on existing but foreign certificate" assert.Fail "azmi getcertificate --certificate ${KV_NA}/certificates/buriedCertificate"
 
 test "getcertificate OK on RO latest PEM certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificate --identity $identity | grep MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCdJzK88AQzWXVO"
+test "getcertificate the same but save to file - relative path" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificate --file download.txt --identity $identity | grep MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCdJzK88AQzWXVO download.txt"
+test "getcertificate the same but absolute path" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificate -f /var/tmp/download.txt --identity $identity | grep MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCdJzK88AQzWXVO /var/tmp/download.txt"
 test "getcertificate OK on RO latest PFX certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat --identity $identity | sha256sum | grep f3f7af1b9fbaa5bfbd54ac21e06af2fb0be37d1766ed11f305f83a55b2623842"
-test "getcertificate OK on RO latest PFX certificate save to file - relative path" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat --file download.txt --identity $identity && sha256sum download.txt | grep f3f7af1b9fbaa5bfbd54ac21e06af2fb0be37d1766ed11f305f83a55b2623842"
-# sha256sum is different due to inserted newline if printed to stdout
-test "getcertificate the same but absolute path" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat -f /var/tmp/download.txt --identity $identity && sha256sum /var/tmp/download.txt | grep 1284f98a73095f554f0dd50b640618520183f11a7961d609dd05c963e35e5056"
+
 
 test "getcertificate OK on RO specific version of PEM certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificate/103a7355c6094bc78307b2db7b85b3c2 --identity $identity | grep MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCvga/z9gy4RG0S"
 test "getcertificate OK on RO specific version of PFX certificate" assert.Success "azmi getcertificate --certificate ${KV_RO}/certificates/readThisCertificatePfxFormat/035f5dc633a445bb801f760f46286785 --identity $identity | sha256sum | grep 1ffd2ed6d8e87997adb4a172b3ff161dd69bba7113a3c16ecc6237cbd749bd0c"
