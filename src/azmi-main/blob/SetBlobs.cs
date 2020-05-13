b2ec078aa1e85734ce54474d1306716fa3498aeb
Igor Iric <iiric@microsoft.com>
2020-05-13 23:58:07 +0200

    initial upload

---
+++ src/azmi-main/blob/SetBlobs.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace azmi_main.blob
+{
+    class SetBlobs : IAzmiCommand
+    {
+        public SubCommandDefinition Definition()
+        {
+            return new SubCommandDefinition
+            {
+
+                name = "setblobs",
+                description = "Writes multiple local files to storage account blobs.",
+
+                arguments = new AzmiArgument[] {
+                    new AzmiArgument("directory","Path to a local directory where local files are located. Examples: /home/avalanche/tmp/ or ./",
+                        required: true),
+                    new AzmiArgument("container","URL of container to which to upload files. Example: https://myaccount.blob.core.windows.net/mycontainer",
+                        required: true),
+                    new AzmiArgument("force", alias: null, type: ArgType.flag,
+                        description: "Overwrite existing blob in Azure."),
+                    new AzmiArgument("exclude", "Exclude blobs that match given regular expression."),
+                    SharedAzmiArguments.identity,
+                    SharedAzmiArguments.verbose
+                }
+            };
+        }
+
+        public class AzmiArgumentsClass : SharedAzmiArgumentsClass
+        {
+            public string directory { get; set; }
+            public string container { get; set; }
+            public string exclude { get; set; }
+            public bool force { get; set; }
+        }
+
+        public List<string> Execute(object options)
+        {
+            AzmiArgumentsClass opt;
+            try
+            {
+                opt = (AzmiArgumentsClass)options;
+            } catch (Exception ex)
+            {
+                throw AzmiException.WrongObject(ex);
+            }
+
+            return Execute(opt.container, opt.directory, opt.identity, opt.exclude, opt.force);
+        }
+
+        public List<string> Execute(string containerUri, string directory, string identity = null, string exclude = null, bool force = false)
+        {
+            // test cases for not existing directory, empty or with one or two files
+            // consider iEnumerable results so it will go out to pipeline
+            foreach (var file in Directory.EnumerateFiles(directory))
+            {
+                // use blob name and not container, blob name should be path after directory name
+                SetBlob.setBlob_byContainer(file, containerUri, identity, force);
+            }
+
+            return new List<string>();
+        }
+    }
+}
