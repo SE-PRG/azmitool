916d9161ee40e7f8e2c93e0b999fe2c92d74dc6b
Igor Iric <iiric@microsoft.com>
2020-05-15 22:09:06 +0200

    recursive diff

--- test/integration/blob-tests.sh
+++ test/integration/blob-tests.sh
@@ -101,7 +101,7 @@ test "setblobs OK with 2 files and force" assert.Success "azmi setblobs -d $UPLO
 mkdir -p "$UPLOAD_DIR/subdirectory" && echo "$DATE2" > "$UPLOAD_DIR/subdirectory/file3.txt"
 test "setblobs OK with subdirectory" assert.Equals "azmi setblobs -d $UPLOAD_DIR -c $CONTAINER_RW --force | wc -l" 3
 rm -rf $DOWNLOAD_DIR
-test "setblobs and getblobs give same files" assert.Success "azmi getblobs -c $CONTAINER_RW -d $DOWNLOAD_DIR --delete-after-copy && diff $UPLOAD_DIR $DOWNLOAD_DIR"
+test "setblobs and getblobs give same files" assert.Success "azmi getblobs -c $CONTAINER_RW -d $DOWNLOAD_DIR --delete-after-copy && diff -r $UPLOAD_DIR $DOWNLOAD_DIR"
 
 
 # mixed commands tests
