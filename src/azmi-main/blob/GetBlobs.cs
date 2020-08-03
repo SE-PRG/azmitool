2d097b19af4be97679ec8a1e977108dbd7dfd787
Igor Iric <iiric@microsoft.com>
2020-08-03 13:39:47 +0200

    re-wrap it in try catch

--- src/azmi-main/blob/GetBlobs.cs
+++ src/azmi-main/blob/GetBlobs.cs
@@ -111,7 +111,7 @@ namespace azmi_main
 
             var results2 = blobListing.AsParallel().Select(blobItem =>
             {
-                //Console.WriteLine($"    start delay {watch.ElapsedMilliseconds - parallelStartTime}");
+                Console.WriteLine($"    start delay {watch.ElapsedMilliseconds - parallelStartTime}");
                 BlobClient blobClient = containerClient.GetBlobClient(blobItem);
                 string filePath = Path.Combine(directory, blobItem);
                 string absolutePath = Path.GetFullPath(filePath);
@@ -119,8 +119,15 @@ namespace azmi_main
                 Directory.CreateDirectory(dirName);
 
 
-                blobClient.DownloadTo(filePath);
-                return $"Success '{blobClient.Uri}'";
+                try
+                {
+                    blobClient.DownloadTo(filePath);
+                    return $"Success '{blobClient.Uri}'";
+                }
+                catch (Exception ex)
+                {
+                    throw AzmiException.IDCheck(identity, ex);
+                }
 
             }).ToList<string>();
 
